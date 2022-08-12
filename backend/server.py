7ae8fd7fd6a3c18982c11af587a50dbc4fd6306e
Sudharsun <psu.fabulo@gmail.com>
2022-08-12 16:31:05 +0530

    server cors | starter

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,20 @@
-from flask_mysqldb import MySQL
 from flask import Flask
 from flask import jsonify
 from flask import request
+
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+
 
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
+
 # import requests
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
