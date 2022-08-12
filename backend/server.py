d8c6f7f48286198c2b02ad6fd398b2e903dea765
Sudharsun <psu.fabulo@gmail.com>
2022-08-12 18:57:35 +0530

    mysql creds | list cutomers & products| -starters

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,13 @@
+from flask import Flask, render_template
+from flask import jsonify
+from flask import request
+
+
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import create_access_token, decode_token
-from flask import Flask, render_template
-from flask import jsonify
-from flask import request
+
 
 from flask_mysqldb import MySQL
 from flask_cors import CORS
@@ -22,6 +25,14 @@ CORS(app)
 
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")  # Change this!
 jwt = JWTManager(app)
+
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "oof"
+
+mysql = MySQL(app)
 
 
 @app.route("/")
@@ -60,6 +71,44 @@ def verify_token():
     """
     return jsonify(auth=True, userName=get_jwt_identity(),)
 # auth ______
+
+
+@app.route("/api/list-customers", methods=['GET'])
+def list_customers():
+    """
+    returns customers list json , to be used in dropdown.
+    this route needs protection.
+    """
+    try:
+        cur = mysql.connection.cursor()
+        customers = cur.execute("SELECT Name , PAN FROM customer")
+
+        if customers > 0:
+            customers_list = cur.fetchall()
+            return jsonify(customers_list)
+
+    except Exception as e:
+        print(f'An exception occurred {e}')
+        return (f'An exception occurred {e}')
+
+
+@app.route("/api/list-products", methods=['GET'])
+def list_products():
+    """
+    returns products list json , to be used in dropdown.
+    this route needs protection.
+    """
+    try:
+        cur = mysql.connection.cursor()
+        products = cur.execute("SELECT * FROM product")
+
+        if products > 0:
+            products_list = cur.fetchall()
+            return jsonify(products_list)
+
+    except Exception as e:
+        print(f'An exception occurred {e}')
+        return (f'An exception occurred {e}')
 
 
 if __name__ == "__main__":
