078303f38a8373656e6d398e5b1e49dd258d6fff
Sudharsun <psu.fabulo@gmail.com>
2022-08-13 01:56:01 +0530

    loadData starter | react query starter

--- frontend/src/pages/AddSubscriptionPage.js
+++ frontend/src/pages/AddSubscriptionPage.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AddSubscriptionLayout from "../layout/AddSubscriptionLayout";
+import { ListCustomers, ListProducts } from "../resources/LoadData";
 
 const AddSubscriptionPage = () => {
+	useEffect(() => {
+		ListCustomers();
+		ListProducts();
+	}, []);
+
 	return <AddSubscriptionLayout />;
 };
 
--- frontend/src/resources/LoadData.js
+++ frontend/src/resources/LoadData.js
@@ -4,7 +4,17 @@ import axios from "axios";
 export const ListCustomers = async () => {
 	try {
 		const response = await axios.get("/api/list-customers");
-		console.log(response);
+		console.log(response.data);
+	} catch (error) {
+		console.error(error);
+	}
+	return;
+};
+
+export const ListProducts = async () => {
+	try {
+		const response = await axios.get("/api/list-products");
+		console.log(response.data);
 	} catch (error) {
 		console.error(error);
 	}
