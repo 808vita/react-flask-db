e1213edc6001121020d3779ffcee93c6027d0222
Sudharsun <psu.fabulo@gmail.com>
2022-08-13 21:12:06 +0530

    freesqldb config

--- backend/server.py
+++ backend/server.py
@@ -26,11 +26,17 @@ CORS(app)
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")  # Change this!
 jwt = JWTManager(app)
 
+# for local db
+# app.config['MYSQL_HOST'] = "localhost"
+# app.config['MYSQL_USER'] = "root"
+# app.config['MYSQL_PASSWORD'] = ""
+# app.config['MYSQL_DB'] = "oof"
 
-app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "oof"
+# freeSqlDB
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
 mysql = MySQL(app)
 
